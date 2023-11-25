17b4e90b7ddcf8e9628a8dcd2ff86a2103c87468
David <david.a.ramirez.chavez-1@ou.edu>
2023-11-25 03:51:28 -0600

    Quiz part (binary multiplication)

--- quizapp/quiz.py
+++ quizapp/quiz.py
@@ -1,38 +1,85 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from quizapp.auth import login_required
 
 from quizapp.db import get_db
 
+from random import randint
+import time
+
 bp = Blueprint('quiz', __name__, url_prefix='/quiz')
 
-@bp.route('/play', methods=('GET', 'POST'))
+result = 0
+count = 0
+numOneBin = ""
+numTwoBin = ""
+
+@bp.route('/play', methods=['GET', 'POST'])
 @login_required
 def play():
-    if request.method == 'POST':
-        score = request.form['score']
+    global numOneBin
+    global numTwoBin
+    global result
+    global count
 
-        db = get_db()
-        error = None
-
-        if not score:
-            error = 'example score required'
+    count = 0
+    
+    if request.method == 'GET':
+        numOne = randint(0,10)
+        numTwo = randint(0,10)
         
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO score (scorer_id, score) VALUES (?, ?)",
-                    (session['user_id'], score),
-                )
-                db.commit()
-            except:
-                error = 'error saving score';
-            return redirect(url_for('index'))
+        numOneBin = bin(numOne)[2:]
+        numTwoBin = bin(numTwo)[2:]
         
-        flash(error)
-
+        result = (numOne * numTwo)
+        
+        return render_template('quiz/play.html', numOneBin = numOneBin, numTwoBin = numTwoBin)
+    
     return render_template('quiz/play.html')
+
+@bp.route('/results')
+def results():
+    global count
+
+    score = count
+
+    db = get_db()
+
+    db.execute("INSERT INTO score (scorer_id, score) VALUES (?, ?)",
+               (session['user_id'], score),
+               )
+    db.commit()
+    
+    return render_template('quiz/results.html', count = count)
+
+
+@bp.route('/count_answer', methods = ["POST"])
+def count_answer():
+    global numOneBin
+    global numTwoBin
+    global result
+    global count
+    
+    suffix = request.form['answer']
+        
+    prefix = "0b"
+    string = prefix + suffix
+    answer = int(string, 2)
+    
+    if answer == result:
+        count = count + 1
+    
+    numOne = randint(0,10)
+    numTwo = randint(0,10)
+    
+    numOneBin = bin(numOne)[2:]
+    numTwoBin = bin(numTwo)[2:]
+    
+    result = numOne * numTwo
+    
+    return render_template('quiz/play.html', numOneBin = numOneBin, numTwoBin = numTwoBin)
+
